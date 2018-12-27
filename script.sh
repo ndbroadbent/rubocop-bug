6b7593f705aacdf3e740106207521e83bd30816b
Nathan Broadbent <git@ndbroadbent.com>
2018-12-27 14:52:03 +0700

    Use current dir so that script can be run from anywhere

--- script.sh
+++ script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+CURRENT_DIR=$(dirname "$0")
 set -x
 
 rubocop
 
 rubocop -c .rubocop.yml
 
-rubocop -c /tmp/rubocop-bug/.rubocop.yml
+rubocop -c "${CURRENT_DIR}/.rubocop.yml"
 
-cat app/models/test_model.rb | rubocop -c /tmp/rubocop-bug/.rubocop.yml \
-    -s /tmp/rubocop-bug/app/models/test_model.rb
+cat app/models/test_model.rb | rubocop -c "${CURRENT_DIR}/.rubocop.yml" \
+    -s "${CURRENT_DIR}/app/models/test_model.rb"
 
-cp app/models/test_model.rb /tmp/test_model.rb
-cat app/models/test_model.rb | rubocop -c /tmp/rubocop-bug/.rubocop.yml \
+cp "${CURRENT_DIR}/app/models/test_model.rb" /tmp/test_model.rb
+cat "${CURRENT_DIR}/app/models/test_model.rb" | rubocop -c "${CURRENT_DIR}/.rubocop.yml" \
     -s /tmp/test_model.rb
